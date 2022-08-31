e25231ec9de036f1335879a39d63d2ef275e7cd4
GiantDole <adrian.koegl@hotmail.com>
2022-08-30 23:47:32 -0400

    added token

--- contracts/ERC20Ticket.sol
+++ contracts/ERC20Ticket.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, ERC20Burnable, Ownable {
+contract ERC20Ticket is ERC20, ERC20Burnable, Ownable {
 
     constructor() ERC20("NFT_Ticketing", "NFTT") {
         mint(msg.sender, 500);
