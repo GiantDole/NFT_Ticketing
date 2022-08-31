89183ef460154a65f3925cf845f089b1bb27aec2
GiantDole <adrian.koegl@hotmail.com>
2022-08-31 02:27:39 -0400

    enabled optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,15 @@ import "solidity-coverage";
 import 'solidity-docgen';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9"
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 20
+      }
+    },
+  }
 };
 
 export default config;
