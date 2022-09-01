4461eae8e8bc91be5a332303298c4412ee62e751
ogatensor <90874464+ogatensor@users.noreply.github.com>
2022-09-01 13:47:13 +0100

    Add functions to grab user, ticket, ticketTypes, events from database

--- backend/src/database/database.service.ts
+++ backend/src/database/database.service.ts
@@ -22,3 +22,34 @@ interface Ticket {
     ipfs_uri: string;
 }
 
+async function getTicket(id: number) : string {
+    const ticket: string; 
+    return ticket; 
+}
+
+async function getEvent(id: number) : string {
+    const event: string; 
+    return event; 
+} 
+
+async function getTicketTypeByEvent(event: string) : Array<string> {
+    const ticketTypes: Array<string>;
+    return ticketTypes;
+} 
+
+async function getTicketTypeByTicketType(ticketType: string) : Array<string> {
+    const ticketTypes: Array<string>;
+    return ticketTypes;
+} 
+
+async function getTicketsOwnedByEvent(event: string) : Array<Ticket> { 
+    const tickets: Array<Ticket>;
+    return tickets;
+} 
+
+async function getTicketsOwnedByUser(user: string) : Array<Ticket> { 
+    const tickets: Array<Ticket>;
+    return tickets;
+} 
+
+
